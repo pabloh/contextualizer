f393cb4cb086d203879f2e47a6cb4164d7ab0cc0
Pablo Herrero <pablodherrero@gmail.com>
2017-06-29 23:30:55 -0300

    Release version 0.0.1

--- lib/contextualizer.rb
+++ lib/contextualizer.rb
@@ -1,5 +1,60 @@
-require "contextualizer/version"
+module Contextualizer
+  def context(*attrs, **opt_attrs)
+    unless @_setter 
+      @_setter = Setter.new
+      include Contextualizer.init_for(@_setter)
+    end
 
-module Contextualized
-  # Your code goes here...
+    @_setter.add_attrs(*attrs, **opt_attrs)
+    attr_reader *attrs, *opt_attrs.keys
+  end
+
+  def self.extended(klass)
+    klass.class_eval do
+      attr_reader :context
+      @_setter = Setter.new
+      include Contextualizer.init_for(@_setter, true)
+    end
+  end
+
+  def self.init_for(setter, first = false)
+    Module.new do |mod|
+      if first 
+        mod.send(:define_method, :initialize) do |args = {}|
+          super()
+          setter.set(self, args)
+        end
+      else
+        mod.send(:define_method, :initialize) do |args = {}|
+          super(args)
+          setter.set(self, args)
+        end
+      end
+    end
+  end
+
+  class Setter
+    def initialize
+      @attrs, @opt_attrs = [], {}
+    end
+
+    def add_attrs(*attrs, **opt_attrs)
+      @attrs |= attrs
+      @opt_attrs.merge!(opt_attrs)
+    end
+
+    def set(obj, args)
+      context = obj.context&.dup || {}
+
+      @opt_attrs.each { |key, val| context[key] = args[key] || val }
+      @attrs.each { |key| context[key] = args[key] }
+
+      context.each do |attr, value|
+        fail ":#{attr} was not found in scope" if value.nil?
+        obj.instance_variable_set :"@#{attr}", value
+      end
+
+      obj.instance_variable_set(:@context, context.freeze)
+    end
+  end
 end
---
+++ spec/contextualizer_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+require 'contextualizer'
+
+describe Contextualizer do
+  describe 'when extending a class' do
+    let(:klass) { Class.new.extend(Contextualizer) }
+
+    it "defines a 'initialize' method which ignores params" do
+      expect(klass.new().context).to eq({})
+      expect(klass.new({}).context).to eq({})
+    end
+
+    context 'with defined context params' do
+      let(:klass) do
+        Class.new do
+          extend Contextualizer
+          context :foo, :bar, quz: 'QUZ'
+        end
+      end
+
+      let(:object) { klass.new(foo: 'FOO', bar: 'BAR', quz: 'CORGE', qux: 'QUX') }
+
+      it "defines 'initialize' method" do
+        expect(object.foo).to eq('FOO')
+        expect(object.bar).to eq('BAR')
+        expect(object.quz).to eq('CORGE')
+      end
+
+      it "defines a 'context' method with the passed values", :aggregate_failures do
+        expect(object.context).to eq(foo: 'FOO', bar: 'BAR', quz: 'CORGE')
+        expect(object.context).to be_frozen
+      end
+
+      context 'and initializing without a non default value' do
+        it 'raise an error' do
+          expect { klass.new(foo: 'FOO') }.to raise_error.
+            with_message(':bar was not found in scope')
+        end
+      end
+
+      context 'and initializing without a default value' do
+        let(:object) { klass.new(foo: 'FOO', bar: 'BAR') }
+
+        it 'sets correct values' do
+          expect(object.foo).to eq('FOO')
+          expect(object.bar).to eq('BAR')
+          expect(object.quz).to eq('QUZ')
+        end
+      end
+
+      context 'and initializing using false as value' do
+        let(:object) { klass.new(foo: false, bar: 'BAR') }
+
+        it 'sets correct values' do
+          expect(object.foo).to eq(false)
+          expect(object.bar).to eq('BAR')
+          expect(object.quz).to eq('QUZ')
+        end
+      end
+
+      context 'and inheriting from that class' do
+        let(:subclass) do
+          Class.new(klass) do
+            context :baz, quux: 'QUUX'
+          end
+        end
+
+        let(:object) { subclass.new(foo: 'FOO', bar: 'BAR', baz: 'BAZ', quz: 'CORGE', qux: 'QUX') }
+
+        it 'uses the new defined params and former ones', :aggregate_failures do
+          expect(object.context).to eq(foo: 'FOO', bar: 'BAR', baz: 'BAZ', quz: 'CORGE',
+                                       quux: 'QUUX')
+
+          expect { subclass.new(foo: 'FOO', bar: 'BAR') }.to raise_error.
+            with_message(':baz was not found in scope')
+        end
+      end
+    end
+
+  end
+end
